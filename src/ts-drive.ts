3c1bc65acff831ed3d3a1f93efbfbb3dcb849c20
KevinRamharak <kevinramharak_95@hotmail.com>
2018-03-27 21:43:07 +0200

    some extra instructions, removed a test and fixed a typo

--- src/ts-drive.ts
+++ src/ts-drive.ts
@@ -3,8 +3,12 @@
  * @description
  * This is a TypeScript library (more like a wrapper) to interact with the DriveApp service from Google Apps Script.
  * It has a fairly simple build system (just concat everything), but does have one rule.
- * This very file is used as the initilialise part of the library and has to be concattenated first.
- * The makefile is already set to use it like that
+ * This very file is used as the initilialise part of the library and has to be concatenated first.
+ * The makefile is already does that:
+ * make [tsc] - runs the typescript compiler
+ * make cat   - runs 'make tsc', concats any '*.js' files into a single file and concats any '*.d.ts' files into a single file
+ * make push  - runs 'make cat' and pushes the './clasp/' directory to the cloud
+ * make clean - removes all generated files
  */
 
 /* This block is meant to add polyfills when needed */
@@ -31,7 +35,3 @@ function iterate<T, R>(iterator: Iterator<T>, func: IteratorCallback<T>, thisArg
     }
     return thisArg;
 }
-
-
-
-const x = new Folder(DriveApp.getRootFolder());
