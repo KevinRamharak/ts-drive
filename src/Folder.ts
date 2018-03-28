f2fc4b7b0868e2f1bab07c5b30c81418a3d01adf
KevinRamharak <kevinramharak_95@hotmail.com>
2018-03-28 15:59:36 +0200

    Folder is done

--- src/Folder.ts
+++ src/Folder.ts
@@ -13,6 +13,27 @@ class Folder extends DriveObject {
         );
     }
 
+    filesByName(name : string) : File[] {
+        return iterate<GoogleAppsScript.Drive.File, File[]>(
+            this.folder.getFilesByName(name),
+            function(this : File[], file: GoogleAppsScript.Drive.File) {
+                this.push(new File(file));
+            },
+            []
+        );
+    }
+
+    fileByType(type : GoogleAppsScript.Base.MimeType) : File[] {
+        return iterate<GoogleAppsScript.Drive.File, File[]>(
+            //@ts-ignore - enum is not declared as string
+            this.folder.getFilesByType(type),
+            function(this : File[], file :GoogleAppsScript.Drive.File) {
+                this.push(new File(file));
+            },
+            []
+        );
+    }
+
     addFile(file: File): this {
         this.folder.addFile(file.file);
         return this;
@@ -20,16 +41,6 @@ class Folder extends DriveObject {
 
     addFiles(files: File[]): this {
         files.forEach(this.addFile, this);
-        return this;
-    }
-
-    addFolder(folder: Folder): this {
-        this.folder.addFolder(folder.folder);
-        return this;
-    }
-
-    addFolders(folders: Folder[]): this {
-        folders.forEach(this.addFolder, this);
         return this;
     }
 
@@ -52,7 +63,74 @@ class Folder extends DriveObject {
         }
     }
 
+    removeFile(file : File) : this {
+        this.folder.removeFile(file.file);
+        return this;
+    }
+
+    get folders() : Folder[] {
+        return iterate<GoogleAppsScript.Drive.Folder, Folder[]>(
+            this.folder.getFolders(),
+            function(this: Folder[], folder: GoogleAppsScript.Drive.Folder) {
+                this.push(new Folder(folder));
+            },
+            []
+        );
+    }
+
+    foldersByName(name : string): Folder[] {
+        return iterate<GoogleAppsScript.Drive.Folder, Folder[]>(
+            this.folder.getFoldersByName(name),
+            function(this: Folder[], folder: GoogleAppsScript.Drive.Folder) {
+                this.push(new Folder(folder));
+            },
+            []
+        );
+    }
+
+    addFolder(folder: Folder): this {
+        this.folder.addFolder(folder.folder);
+        return this;
+    }
+
+    addFolders(folders: Folder[]): this {
+        folders.forEach(this.addFolder, this);
+        return this;
+    }
+
     createFolder(name: string): Folder {
         return new Folder(this.folder.createFolder(name));
+    }
+
+    removeFolder(folder : Folder) : this {
+        this.folder.removeFolder(folder.folder);
+        return this;
+    }
+
+    //@TODO: some kind of pattern builder|factory class
+    searchFiles(pattern : string) : File[] {
+        return iterate<GoogleAppsScript.Drive.File, File[]>(
+            this.folder.searchFiles(pattern),
+            function(this: File[], file : GoogleAppsScript.Drive.File) {
+                this.push(new File(file));
+            },
+            []
+        );
+    }
+
+    searchFolders(pattern : string) : Folder[] {
+        return iterate<GoogleAppsScript.Drive.Folder, Folder[]>(
+            this.folder.searchFolders(pattern),
+            function(this : Folder[], folder : GoogleAppsScript.Drive.Folder) {
+                this.push(new Folder(folder));
+            },
+            []
+        );
+    }
+
+    search(pattern : string) : (File|Folder)[] {
+        const folders = this.searchFolders(pattern);
+        const files = this.searchFiles(pattern);
+        return (<(Folder|File)[]>folders).concat(files);
     }
 }
